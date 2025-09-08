02d30e1291b62fd4e479777471badb0dec5818f3
pujitha0907 <chayammagari@karunya.edu.in>
2025-09-08 21:03:21 +0530

    Update README.md

--- README.d
+++ README.d
@@ -1,4 +1,5 @@
 # OOPS--EXP5
 
 #WorkerPayDemo
-<img width="672" height="680" alt="Image" src="https://github.com/user-attachments/assets/9d745f6e-7c6f-431b-bb4e-a24aab1dbcf8" />
+
+<img width="672" height="680" alt="Image" src="https://github.com/user-attachments/assets/252b4c9f-e17e-47da-bb5a-1572814f3b07" />
